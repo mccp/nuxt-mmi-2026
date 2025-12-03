3f49b4d23c732f2e0b313921f2593970851a54c9
mccp <parent.clement2@gmail.com>
2025-12-03 09:24:05 +0100

    [fix] - Lint

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -11,7 +11,7 @@ function onLogoutClick () {
 
 const config = useRuntimeConfig()
 
-const { data } = await useAsyncData('my-recipes', () => {
+await useAsyncData('my-recipes', () => {
   const cookie = useCookie('recipe_token')
   return $fetch(`${config.public.apiUrl}/api/recipes/my-recipes`, {
     headers: {
